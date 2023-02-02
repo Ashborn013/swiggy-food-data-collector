6839a3af264be288f03838696e4411ed29e02aa1
Arjun013H <csarjun11@gmail.com>
2023-02-02 12:46:07 +0530

    more formating

--- heraf.py
+++ heraf.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import pickle
 
-url = 'https://www.swiggy.com'
+url = "https://www.swiggy.com"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 item__new_url = doc.find_all(class_="_3TjLz b-Hy9")
@@ -12,20 +12,20 @@ shop_curls = []
 
 
 for i in item__new_url:
-    l_cits.append(f'https://www.swiggy.com/city/{i.text}')#get all the citys in the site
+    l_cits.append(
+        f"https://www.swiggy.com/city/{i.text}"
+    )  # get all the citys in the site
 
 for url in l_cits:
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-    shop_class_url = doc.find_all(href=True, class_='_1j_Yo')
+    shop_class_url = doc.find_all(href=True, class_="_1j_Yo")
     for i in shop_class_url:
-        the_we_url = i['href']
+        the_we_url = i["href"]
         shop_curls.append(f"https://www.swiggy.com{the_we_url}")
         # print(shop_curls[-1])
 
-
     # if len(shop_curls) > 500:
     #     break
-with open('urls.pkl', 'wb') as f:
+with open("urls.pkl", "wb") as f:
     pickle.dump(shop_curls, f)
-    
